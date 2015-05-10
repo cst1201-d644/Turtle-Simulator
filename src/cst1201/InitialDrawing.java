1dd0ec52599330f63720f914fed71f11d1392716
Winnie <Winnie@Winnie-PC>
2015-05-10 15:34:44 -0400

    Made some changes to InitialDrawing

---
+++ src/cst1201/InitialDrawing.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cst1201;
+
+/**
+ *
+ * @author Winnie
+ */
+public class InitialDrawing {
+    public void draw(){
+        Turtle myTurtle = new Turtle();
+        myTurtle.left(90);
+        myTurtle.penUp();
+        myTurtle.forward(300);
+        myTurtle.penDown();
+        myTurtle.right(180);
+        myTurtle.forward(300);
+        myTurtle.left(155);
+        myTurtle.forward(260);
+        myTurtle.right(130);
+        myTurtle.forward(260);
+        myTurtle.left(155);
+        myTurtle.forward(300);
+        
+        myTurtle.right(90);
+        myTurtle.penUp();
+        myTurtle.forward(40);
+        myTurtle.right(90);
+        myTurtle.forward(150);
+        myTurtle.left(90);
+        myTurtle.penDown();
+        myTurtle.forward(70);
+        myTurtle.left(180);
+        myTurtle.penUp();
+        myTurtle.forward(35);
+        myTurtle.right(90);
+        myTurtle.forward(35);
+        myTurtle.right(180);
+        myTurtle.penDown();
+        myTurtle.forward(70);
+        
+        myTurtle.left(90);
+        myTurtle.penUp();
+        myTurtle.forward(95);
+        myTurtle.left(90);
+        myTurtle.forward(190);
+        myTurtle.right(180);
+        myTurtle.penDown();
+        myTurtle.forward(300);
+        
+    }
+}
