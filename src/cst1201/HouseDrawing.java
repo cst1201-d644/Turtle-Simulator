0851b4d09869f27918f32071c3dbece3a4de798c
class <class@CST_N417_03.cst.citytech.cuny.edu>
2015-05-11 17:34:58 -0400

    comitting 3

---
+++ src/cst1201/HouseDrawing.java
@@ -0,0 +1,32 @@
+package cst1201;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author class
+ */
+public class HouseDrawing {
+
+    public static void main(String[] argsd) {
+        Turtle myTurtle = new Turtle();
+        myTurtle.setPenColor("pink");
+        myTurtle.left(90);
+        myTurtle.forward(100);
+        myTurtle.right(90);
+        myTurtle.forward(100);
+        myTurtle.right(90);
+        myTurtle.forward(100);
+        myTurtle.right(90);
+        myTurtle.forward(100);
+        myTurtle.right(90);
+        myTurtle.forward(100);
+        myTurtle.right(45);
+        myTurtle.forward(100);
+        myTurtle.right(90);
+        myTurtle.forward(Math.sqrt(71));
+    }
+}
--- src/cst1201/InitialDrawing.java
+++ src/cst1201/InitialDrawing.java
@@ -9,9 +9,9 @@ public class InitialDrawing {
         
         myTurtle.left (90);
         myTurtle.left(180);
-        myTurtle.forward(300);
-        myTurtle.left(90);
         myTurtle.forward(200);
+        myTurtle.left(90);
+        myTurtle.forward(100);
         myTurtle.setPenColor("white");
         myTurtle.forward (100);
         myTurtle.left(90);
@@ -34,8 +34,16 @@ public class InitialDrawing {
         myTurtle.left(90);
         myTurtle.left(75);
         myTurtle.setPenColor("green");
-        myTurtle.forward(300);
+        myTurtle.forward(200);
         myTurtle.right(90);
+        myTurtle.right(60);
+        myTurtle.forward(200);
+        myTurtle.right(180);
+        myTurtle.forward(100);
+        myTurtle.left(75);
+        myTurtle.forward(50);
+        
+        
         
          
         
