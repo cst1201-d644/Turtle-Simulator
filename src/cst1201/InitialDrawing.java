ecae3e72a5f012e92e594b0fc0b5539b70d13335
laboni <laboni@laboni-THINK.home>
2015-05-11 01:08:22 -0400

    comitting progress on turtle

---
+++ src/cst1201/InitialDrawing.java
@@ -0,0 +1,13 @@
+package cst1201;
+
+public class InitialDrawing {
+     public static void main(String[] argsd) {
+
+   // public void draw() {
+        Turtle myTurtle = new Turtle();
+        myTurtle.setPenColor("blue");
+        myTurtle.right(300); 
+        
+    }
+}
+//}
