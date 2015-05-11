82890210a4468d0ab16c318147d5640c883dde8a
class <class@CST_N919_06.cst.citytech.cuny.edu>
2015-05-11 19:05:36 -0400

    Finish Part 1

--- src/cst1201/Bowtie.java
+++ src/cst1201/Bowtie.java
@@ -3,8 +3,22 @@ package cst1201;
 /**
  * A class representing a Bowtie drawn by a Turtle object.
  *
- * @author (your name)
+ * @author (Niaz Morshed and Devaya Gurung)
  */
 public class Bowtie {
-
+    
+   // The draw method draw a bowtie
+    public void draw() {
+        Turtle myTurtle = new Turtle();
+        myTurtle.setPenColor("blue");
+        myTurtle.left(90);
+        myTurtle.forward(300);
+        myTurtle.right(135);
+        myTurtle.forward(425);
+        myTurtle.left(135);
+        myTurtle.forward(300);
+        myTurtle.left(135);
+        myTurtle.forward(425);
+        
+    }
 }
