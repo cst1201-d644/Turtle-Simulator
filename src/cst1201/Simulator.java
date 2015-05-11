7b4252a9d960c0a8108603fd5532e6ff52262545
class <class@CST_N904_08.cst.citytech.cuny.edu>
2015-05-11 14:34:51 -0400

    Finished Part 1

---
+++ src/cst1201/Simulator.java
@@ -0,0 +1,19 @@
+/*
+Create a mian method for the whole package
+ */
+
+package cst1201;
+
+/**
+ *
+ * @author class
+ */
+public class Simulator {
+    public static void main(String[] agrs) {
+        Bowtie turtle=new Bowtie();
+        turtle.draw();
+        
+    }
+
+    
+}
