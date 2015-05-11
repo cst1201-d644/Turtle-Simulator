d975a11339213cceec3ff0c3fd9bcd93150cc909
class <class@CST_N904_17.cst.citytech.cuny.edu>
2015-05-11 15:32:16 -0400

    finished part 2

---
+++ src/cst1201/InitialDrawing.java
@@ -0,0 +1,26 @@
+package cst1201;
+
+/**
+ *
+ * @author class
+ */
+public class InitialDrawing {
+
+    public void draw() {
+
+        Turtle myTurtle = new Turtle();
+
+        myTurtle.setPenColor("green");
+        myTurtle.left(90);
+        myTurtle.forward(100);
+        myTurtle.left(45);
+        myTurtle.forward(50);
+        myTurtle.left(180);
+        myTurtle.forward(50);
+        myTurtle.left(90);
+        myTurtle.forward(50);
+
+       
+
+    }
+}
