5731e62dce72a12480d278e43e74a2d1195bb98f
programming <programming@ALC-F07.alc.local>
2015-05-12 13:12:36 -0400

    Finished Lab6

--- src/cst1201/Bowtie.java
+++ src/cst1201/Bowtie.java
@@ -1,10 +1,48 @@
 package cst1201;
 
+import cst1201.Turtle;
+
 /**
  * A class representing a Bowtie drawn by a Turtle object.
  *
- * @author (your name)
+ * @author Sameen Qaiser
  */
 public class Bowtie {
+    private static Object myTurtle;
+ 
+    public static void main(String[] args) {
+         
+         Turtle turtle = new Turtle();
+        turtle.forward(200);
+        turtle.left(90);
+        turtle.forward(200);
+        turtle.left(90);
+        turtle.forward(200);
+        turtle.left(90);
+        turtle.forward(200);
+        turtle.left(180);
+        turtle.penUp();
+        turtle.forward(200);
+        turtle.penDown();
+        turtle.right(45);
+        turtle.forward(150);
+        turtle.right(90);
+        turtle.forward(150);
+        
+       
+        
+          
+       
+       
+        
+
+  
+
+       
+
+       
+
+    }
 
 }
+
